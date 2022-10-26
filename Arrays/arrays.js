b3dd0f84ee9a2c49bf90497a7e0b8cbcd58e8dd1
Shruti <shrutileena@gmail.com>
2022-10-26 16:39:02 +0530

    added changes

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -14,3 +14,13 @@ console.log(groceries.length)
 
 // indexOf
 console.log(groceries.indexOf('banana'))
+
+// Array methods (Higher order functions) : map(), filter(), reduce()
+// arguments of these methods are functions as well
+// callback function
+
+// map()
+const nums = [1,2,3,4]
+nums.map(number => console.log(number))
+const result = nums.map(number => number * 2)
+console.log(result)
